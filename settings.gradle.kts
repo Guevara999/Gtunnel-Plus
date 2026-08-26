9f8a429d020ddbf3d8c2c82332f8b7399d2a91a5
Guevara999 <cheguevara3727@gmail.com>
2026-08-26 06:03:00 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,21 +7,15 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    // Use PREFER_SETTINGS so project repos can supplement
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
-        // JitPack – for libsu, compose-markdown, etc.
         maven { url = uri("https://jitpack.io") }
-        // Nekohasekai – for libghostty (if kept) and other SagerNet artifacts
         maven { url = uri("https://maven.nekohasekai.io/repository/maven-public/") }
-        // Snapshots for SNAPSHOT versions
         maven { url = uri("https://maven.nekohasekai.io/repository/maven-snapshots/") }
     }
 }
 
 rootProject.name = "Gtunnel-Plus"
 include(":app")
-// If you have other modules, include them here
-// include(":libxposed-api")
